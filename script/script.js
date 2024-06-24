19f9238332348ef7a496d07ec95e28705a3de8be
Ruan <ruan.s.gonzalez@gmail.com>
2024-06-24 11:40:24 -0300

    renomeado o contact.html para form.html e alteado o formaction.html

--- script/script.js
+++ script/script.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     checkScrollPosition();
 });
 function getParameters() {
-
+    
     var params = new Array();
     var paramsRet = new Array();
     var url = window.location.href;
@@ -80,3 +80,34 @@ function getParameters() {
     }
     return null;
 }
+function mascaraTelefone(event) {
+    let tecla = event.key;
+    let telefone = event.target.value.replace(/\D+/g, "");
+    
+    if (/^[0-9]$/i.test(tecla)) {
+    telefone = telefone + tecla;
+    let tamanho = telefone.length;
+    
+    if (tamanho >= 12) { 
+    return false;
+    }
+    
+    if (tamanho > 10) {
+    telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (tamanho > 5) {
+    telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (tamanho > 2) {
+    telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+    telefone = telefone.replace(/^(\d*)/, "($1");
+    }
+    
+    event.target.value = telefone;
+    }
+    
+    if (!["Backspace", "Delete", "Tab"].includes(tecla)) {
+    return false;
+    }
+    }
+
+
